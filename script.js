595d9e94713603127b622b88f6015b46fdf92287
Parmar Janavi <janaviparmar370@gmail.com>
2026-06-29 12:38:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -839,7 +839,7 @@ function renderContactInfo(contactInfo, personalInfo) {
   if (!container) return;
 
   const socialLinks = {
-    linkedin: "www.linkedin.com/in/janavi-parmar-45a5b02b0",
+    linkedin: "https://www.linkedin.com/in/janavi-parmar-45a5b02b0",
     instagram: "",
     github: "https://github.com/Janavi-25"
   };
